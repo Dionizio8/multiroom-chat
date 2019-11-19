3db0b681295ae547279258f716f2b6154454e1a9
Gabriel Dionizio Pereira <dionizio.0808@gmail.com>
2019-11-18 22:24:00 -0300

    redirect directory all assets

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine','ejs');
-app.set('views','../view/');
+app.set('views','server/view/');
 
-app.use(express.static('../../public/assets'));
+app.use(express.static('public/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('./route'));
